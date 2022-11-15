fa313fa5b5cd320924adb467a0916cd48e012407
Tommaso Severi <tommaso.severi2@studio.unibo.it>
2022-11-15 22:40:40 +0100

    Completed es. 82

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +18,45 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
 
+    private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
+
+    public SimpleGUI() {
+        frame.setTitle("My first Java graphical interface");
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea();
+        final JButton save = new JButton("Save");
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    controller.writeFile(text.getText());
+                } catch (IOException e) {
+                    System.out.println("GUI controller encountered an error");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,7 +1,19 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,6 +21,71 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
 
+    private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
+
+    public SimpleGUIWithFileChooser() {
+        frame.setTitle("My second Java graphical interface");
+        final JPanel up = new JPanel();
+        final JPanel bottom = new JPanel();
+        up.setLayout(new BorderLayout());
+        bottom.setLayout(new BorderLayout());
+        final JTextField filePath = new JTextField(controller.getFile().getPath());
+        filePath.setEditable(false);
+        final JButton browse = new JButton("Browse...");
+        final JTextArea text = new JTextArea();
+        final JButton save = new JButton("Save");
+        up.add(filePath);
+        up.add(browse, BorderLayout.LINE_END);
+        bottom.add(up, BorderLayout.NORTH);
+        bottom.add(text, BorderLayout.CENTER);
+        bottom.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(bottom);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    controller.writeFile(text.getText());
+                } catch (IOException e) {
+                    System.out.println("GUI controller encountered an error");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        browse.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                final JFileChooser chooseFile = new JFileChooser("Select a new file");
+                switch (chooseFile.showSaveDialog(frame)) {
+                    case JFileChooser.APPROVE_OPTION:
+                        controller.setFile(chooseFile.getSelectedFile());
+                        filePath.setText(chooseFile.getSelectedFile().getPath());
+                        break;
+                    case JFileChooser.CANCEL_OPTION:
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(frame, "An error has occurred");
+                        break;
+                }
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUIWithFileChooser().display();
+    }
 }
